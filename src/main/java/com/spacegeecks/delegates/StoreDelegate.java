fcf5464ad764866557107de3ff8d78076c32480a
sleepygibbs <gibbs041398@gmail.com>
2020-10-07 12:46:20 -0400

    parse double

--- src/main/java/com/spacegeecks/delegates/StoreDelegate.java
+++ src/main/java/com/spacegeecks/delegates/StoreDelegate.java
@@ -32,20 +32,19 @@ public class StoreDelegate implements FrontControllerDelegate {
 				Map<String, Object> jsonMap = om.readValue(request.getInputStream(), Map.class);
 				
 				if (jsonMap.containsKey("price") && jsonMap.containsKey("listing_id") && jsonMap.containsKey("title") && jsonMap.containsKey("image")) {
-					
-					t.setPrice((double) jsonMap.get("price"));
+
+					t.setPrice(Double.parseDouble((String) jsonMap.get("price")));
 					t.setListingId(((Integer) jsonMap.get("listing_id")).toString());
 					t.setTitle((String) jsonMap.get("title"));
 					t.setImage((String) jsonMap.get("image"));
 					
 					t.setUserId(uSession.getUserId());
+
 					
 					TransactionStatus ts = new TransactionStatus();
 					ts = tServ.findTStatusByName("open");
 					
 					t.setId(tServ.addToCart(uSession, t));
-					
-					System.out.println(t.toString());
 					
 					response.getWriter().write(om.writeValueAsString(t));
 				} else {
