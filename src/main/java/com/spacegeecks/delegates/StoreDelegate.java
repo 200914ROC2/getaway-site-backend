cd8d6de0e5658c5883d8fa74c0f286ef311f7887
sleepygibbs <gibbs041398@gmail.com>
2020-10-07 22:55:09 -0400

    system logs

--- src/main/java/com/spacegeecks/delegates/StoreDelegate.java
+++ src/main/java/com/spacegeecks/delegates/StoreDelegate.java
@@ -46,6 +46,9 @@ public class StoreDelegate implements FrontControllerDelegate {
 					
 					t.setId(tServ.addToCart(uSession, t));
 					
+					System.out.println("The u Session "+uSession);
+					System.out.println("The t object "+t);
+					System.out.println("The jsonMap"+jsonMap);
 					response.getWriter().write(om.writeValueAsString(t));
 				} else {
 					response.sendError(400, "Field listing error.");
